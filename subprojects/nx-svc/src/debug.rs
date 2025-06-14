d11c39dbe37ccb636baa33aa0089d6d50a817af9
Lorenzo Delgado <lnsdev@proton.me>
2025-06-14 19:19:38 +0200

    feat(nx-svc): add memory management module
    - Introduced a new `mem` module for memory management system calls and utilities.
    - Implemented functions for querying and unmapping memory regions, along with error handling.
    - Added `MemoryInfo`, `MemoryState`, `MemoryType`, `MemoryAttribute`, and `MemoryPermission` structures for detailed memory management.
    - Integrated `bitflags` for managing memory state and attributes.
    - Updated `Cargo.toml` to include the `bitflags` dependency.
    - Refactored existing code to accommodate new memory management functionalities.
    
    Signed-off-by: Lorenzo Delgado <lnsdev@proton.me>

--- subprojects/nx-svc/src/debug.rs
+++ subprojects/nx-svc/src/debug.rs
@@ -1,14 +1,9 @@
-use core::ffi::c_void;
-
 use super::raw::__nx_svc_break;
+// TODO: Do not re-export the raw type
 pub use super::raw::BreakReason;
 
 // TODO: Review if it must be -> !
-pub unsafe fn break_event(
-    reason: BreakReason,
-    address: *mut c_void, // TODO: Review uintptr_t
-    size: usize,          // TODO: Review uintptr_t
-) -> Result<(), ()> {
+pub unsafe fn break_event(reason: BreakReason, address: usize, size: usize) -> Result<(), ()> {
     let res = unsafe { __nx_svc_break(reason, address, size) };
     if res == 0 { Ok(()) } else { Err(()) }
 }
